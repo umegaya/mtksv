666258d1060452ca66fec0514aadf3a46b371261
Takehiro Iyatomi <iyatomi@gmail.com>
2017-06-23 15:51:10 +0900

    update submodule and related changes

--- container/image/dotnet/build.sh
+++ container/image/dotnet/build.sh
@@ -8,6 +8,8 @@ ROOT=$SCPDIR/../../..
 OUTDIR=$ROOT/build/dotnet/server
 
 mkdir -p $OUTDIR
+echo "docker run --rm -ti -v $1:/codes/Server -v $ROOT/ext/mtk/bindings/csharp:/codes/Mtk -v $ROOT/tools/dotnet:/mtk/bin -v $OUTDIR:/tmp/out \
+	mtktool/builder-dotnet make -C /mtk/bin lib"
 docker run --rm -ti -v $1:/codes/Server -v $ROOT/ext/mtk/bindings/csharp:/codes/Mtk -v $ROOT/tools/dotnet:/mtk/bin -v $OUTDIR:/tmp/out \
 	mtktool/builder-dotnet make -C /mtk/bin lib
 mv $OUTDIR/libserver.so $SCPDIR
